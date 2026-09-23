c2a9ad2e04a4bc6ce17b8ed0d08b818c96258792
Boebel-Iago <iagolboebel@gmail.com>
2026-09-22 23:35:39 -0300

    fix(api): update websocket cors to allow all origins via setAllowedOriginPatterns

--- api/src/main/java/com/boebel/api/config/WebSocketConfig.java
+++ api/src/main/java/com/boebel/api/config/WebSocketConfig.java
@@ -24,6 +24,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200");
+                .setAllowedOriginPatterns("*");
     }
 }
